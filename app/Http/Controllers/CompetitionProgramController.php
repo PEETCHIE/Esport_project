7ca26a42e07b25edca5df0432052bd1970413ecc
PEETCHIE <chaianan.si@rmuti.ac.th>
2024-02-12 16:49:46 +0700

    CHANGE

--- app/Http/Controllers/CompetitionProgramController.php
+++ app/Http/Controllers/CompetitionProgramController.php
@@ -159,19 +159,17 @@ class CompetitionProgramController extends Controller
 
     public function showProgram($id){
         $programs = DB::table('competition_programs')->where('cl_id', $id)->pluck('id')->toArray();
+        // dd($all_programs);
         $buckets = [];
-
         foreach ($programs as $program) {
             $bucket = DB::table('tournament_in_teams')->where('cp_id', $program)
                 ->join('teams', 'tournament_in_teams.t_id', '=', 'teams.id')
                 ->join('competition_programs', 'tournament_in_teams.cp_id', '=', 'competition_programs.id')
-                ->select('t_name','logo','matches')
+                ->select('t_name','logo','matches', 'round', 'cp_id')
                 ->get();
             $buckets[] = $bucket;
         }
-
         // dd($buckets);
-
         return view('manager.competition_program', compact('buckets'));
     }
 
@@ -202,17 +200,31 @@ class CompetitionProgramController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(competition_program $competition_program)
+    public function edit($id)
     {
         //
+        $cp_edit = DB::table('competition_programs')->WHERE('id', $id)->first();
+        // dd($cp_edit); 
+        return view('manager.competition_program_edit', compact('cp_edit'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Updatecompetition_programRequest $request, competition_program $competition_program)
+    public function update(Updatecompetition_programRequest $request, competition_program $competition_program, $id)
     {
         //
+        $data = DB::table('competition_programs')->WHERE('id', $id)->update([
+            'match_date'=> $request->match_date,
+            'match_time'=> $request->match_time,
+        ]);
+        // dd($data);
+        return redirect()->route('managers_competition.index')->with('alert', [
+            'icon' => 'success
+            ',
+            'title' => 'Your success message',
+            'text' => 'แก้ไขข้อมูลเรียบร้อยแล้ว',
+        ]);
     }
 
     /**
--- resources/views/manager/competition_program.blade.php
+++ resources/views/manager/competition_program.blade.php
@@ -5,33 +5,64 @@
         </h2>
     </x-slot>
 
-    <div class="grid grid-cols-row">
-        @foreach($buckets as $bucket)
+    @foreach($buckets as $bucket)
+        <div class="grid grid-cols-rows">
             <div class="mx-8 w-48 grid-cols-3 gap-3">
-                @foreach($bucket as $item)
+                @php $buttonDisplayed = false; @endphp
+                @foreach($bucket as $index => $item)
+                    @if($item->round == 'R1')
+                        <b>{{$item->matches}}</b>
+                        <div class="border rounded bg-[#01142E] border-[#01142E] text-white flex justify-between grid-cols-2 gap-2">
+                            <div class="flex items-center justify-center mx-auto">
+                            <img src="{{ $item->logo }}" class="w-5 h-5" alt="">
+                                <span class="ml-2">
+                                        {{ $item->t_name }} 
+                                </span>
+                            </div> 
+                            <div class="border bg-white text-black flex items-center justify-center w-10">
+                                0
+                            </div>
+                        </div>
+                        @if($index == 0 && !$buttonDisplayed)
+                        <form action="{{ route('edit_competition_program', $item->cp_id)}}">
+                        <div class="absolute transform translate-x-1 -translate-y-2">
+                            <button type="submit" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-[200px]">edit</button>
+                        </div>
+                    </form>
+                        @php $buttonDisplayed = true; @endphp
+
+                        @endif
+                    @endif
+                    @if($item->round == 'R2')
+                    {{-- <br>
                     <b>{{$item->matches}}</b>
                     <div class="border rounded bg-[#01142E] border-[#01142E] text-white flex justify-between grid-cols-2 gap-2">
                         <div class="flex items-center justify-center mx-auto">
-                            <img src="{{ $item->logo }}" class="w-5 h-5" alt="">
+                        <img src="{{ $item->logo }}" class="w-5 h-5" alt="">
                             <span class="ml-2">
-                                {{ $item->t_name }}  
+                                    {{ $item->t_name }} 
                             </span>
-                        </div>
+                        </div> 
                         <div class="border bg-white text-black flex items-center justify-center w-10">
                             0
                         </div>
                     </div>
-                    @endforeach
-                    <div class="absolute transform translate-x-1 -translate-y-12">
-                        <button type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-[200px]">edit</button>
-                    </div><br>
-                </div>
-                 
-        @endforeach
-    </div>
+                        @if($index == 0 && !$buttonDisplayed)
+                        <div class="absolute transform translate-x-1 -translate-y-2">
+                            <button type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-[200px]">edit</button>
+                        </div>
+                        @php $buttonDisplayed = true; @endphp
+                        @endif --}}
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    @endforeach
+
+    
 </x-app-layout>
 
-<!-- <script>
+<script>
         @if(session('alert'))
             Swal.fire({
                 icon: '{{ session('alert')['icon'] }}',
@@ -39,4 +70,4 @@
                 text: '{{ session('alert')['text'] }}',
             });
         @endif
-</script> -->
+</script> 
